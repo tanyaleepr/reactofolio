e3c0e10d98fd0d7b08fb1706ff2864a361083736
tanyaleepr <tanyaleepr@gmail.com>
2022-04-24 20:42:15 -0500

    added about me

---
+++ src/components/About/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+function About() {
+	return (
+		<section>
+			<div className="center" id="about">
+				<h1 className="page-header">About Me</h1>
+			</div>
+			<div className="center">
+				<img
+					src={require('../../assets/images/1631889415343 (1).jfif)}
+					alt="about-me"
+					className="photo"
+				/>
+			</div>
+			<div>
+				<p>
+				Hi, I'm Tanya Lee and I run this website to provide small businesses or independent entrepreneurs like yourself with high quality business services and resources. Let me show you how to grow your business with my website services.
+				</p>
+				
+			</div>
+		</section>
+	);
+}
+
+export default About;
