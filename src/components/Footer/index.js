ea3c850fb5cde8eaaf0dbb4a06539917fcf95f16
tanyaleepr <tanyaleepr@gmail.com>
2022-04-24 20:36:46 -0500

    added footer

---
+++ src/components/Footer/index.js
@@ -0,0 +1,49 @@
+import React from "react";
+
+function Footer() {
+	return (
+		<footer className="footer">
+			<div>
+				<a
+					href="https://github.com/tanyaleepr"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					<img
+						src={require("../../assets/logos/648256.png")}
+						alt="Github"
+						className="logo"
+					></img>
+				</a>
+			</div>
+			<div>
+				<a
+					href="https://www.linkedin.com/in/tanyagonzalez/"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					<img
+						src={require("../../assets/logos/new-linkedin-logo-white-black-png.png")}
+						alt="LinkedIn"
+						className="logo"
+					></img>
+				</a>
+			</div>
+			<div>
+				<a
+					href="https://twitter.com/tanyaleepr"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					<img
+						src={require("../../assets/logos/87390.png")}
+						alt="Twitter"
+						className="logo"
+					></img>
+				</a>
+			</div>
+		</footer>
+	);
+}
+
+export default Footer;
